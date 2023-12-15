bcdfc75d404b8cb665c560f68febc883932ee756
Dimitris Dafnoudis <138138270+Ddafnoudis@users.noreply.github.com>
2023-12-15 21:55:56 +0200

    Update scRNA-seq.R

--- scRNA-seq.R
+++ scRNA-seq.R
@@ -4,8 +4,7 @@ library(ggplot2)
 library(tidyverse)
 library(gridExtra)
 
-setwd("C:/Users/dimit/Desktop/12342")
-getwd()
+setwd("C:/Users/dimit/Desktop/scRNA_seq/")
 
 directories <- list.dirs(path = 'data/', recursive = F, full.names = F)
 
@@ -13,24 +12,25 @@ directories <- list.dirs(path = 'data/', recursive = F, full.names = F)
 # it should go in each folder and fetch
 # the files to create counts matrix
 # and then using the count matrix
-# we create a Seurat object and assign it
+# we create a Seurat object and assign
 # a variable that has both the patient 
 # information as well as the type of the tissue.
+
 ### Remember that it takes some time to loop all the folders ###
 
 for(x in directories){
   name <- gsub('_filtered_feature_bc_matrix','', x)
-
+  
   counts <- ReadMtx(mtx = paste0('data/',x,'/matrix.mtx.gz'),
-                 features = paste0('data/',x,'/features.tsv.gz'),
-                 cells = paste0('data/',x,'/barcodes.tsv.gz'))
-
+                    features = paste0('data/',x,'/features.tsv.gz'),
+                    cells = paste0('data/',x,'/barcodes.tsv.gz'))
+  
   # create seurat objects
   assign(name, CreateSeuratObject(counts = counts))
 }
 
 
-# Merge these objects together into one object
+## Merge these objects together into one object
 # Why we merge them?
 # Because there is a big chance to 
 # make mistakes by proceeding to the 
@@ -60,13 +60,18 @@ merge_seurat
 
 view(merge_seurat@meta.data)
 
-# Based on the information now 
-# I want to create a column with what patient and
-# tissue is the cell barcode origin from.
+# based on the information now 
+# I want to create a column that
+# will tell me what patient and
+# tissue is the cell barcode origin
+# from.
 # Create a sample column
 
 
-merge_seurat$sample <- rownames(merge_seurat@meta.data)
+merge_seurat$sample <- rownames(
+  merge_seurat@meta.data
+  )
+
 view(merge_seurat@meta.data)
 
 
@@ -91,7 +96,10 @@ unique(merge_seurat@meta.data$Tissue)
 # The seurat function PercentageFeatureSet()
 # enables you to calculate the mitochondrial transcript
 
-merge_seurat$mitoPer <- PercentageFeatureSet(merge_seurat, pattern = "^MT-")
+merge_seurat$mitoPer <- PercentageFeatureSet(
+  merge_seurat, pattern = "^MT-"
+  )
+
 view(merge_seurat$mitoPer)
 
 ## EXPLORE THE DATA##
@@ -105,23 +113,78 @@ view(merge_seurat$mitoPer)
 # mitochondrial counts were removed
 
 merge_seurat_filtered <- subset(merge_seurat, subset = nCount_RNA > 800 &
-       nFeature_RNA > 500 &
-         mitoPer < 10)
+                                  nFeature_RNA > 500 &
+                                  mitoPer < 10)
 merge_seurat_filtered
 
-# How can I find how many genes I had before filtering?
+# how can i find how many genes
+# I had before filtering?
 
 merge_seurat
 
 
-# Now we want to figure out if we have any batch effects
-# Normalization
+# Now we want to figure if we have
+# any batch effects
 
 
-merge_seurat_filtered <- NormalizeData(object = merge_seurat_filtered)
-merge_seurat_filtered <- FindVariableFeatures(object = merge_seurat_filtered)
-merge_seurat_filtered <- ScaleData(object = merge_seurat_filtered)
-merge_seurat_filtered <- RunPCA(object = merge_seurat_filtered)
+merge_seurat_filtered <- NormalizeData(
+  object = merge_seurat_filtered
+  )
+
+merge_seurat_filtered <- FindVariableFeatures(
+  object = merge_seurat_filtered
+  )
+
+merge_seurat_filtered <- ScaleData(
+  object = merge_seurat_filtered
+  )
+
+merge_seurat_filtered <- RunPCA(
+  object = merge_seurat_filtered
+  )
+
 # find the dimension of the dataset
 ElbowPlot(merge_seurat_filtered)
-merge_seurat_filtered <-FindNeighbors(object = merge_seurat_filtered)
+merge_seurat_filtered <- FindNeighbors(object = merge_seurat_filtered, dims = 1:20)
+merge_seurat_filtered <- FindClusters(object = merge_seurat_filtered)
+merge_seurat_filtered <- RunUMAP(object = merge_seurat_filtered, dims = 1:20)
+
+
+
+# plot
+p1 <- DimPlot(merge_seurat_filtered, reduction = 'umap', group.by = 'Patient_ID')
+p2 <- DimPlot(merge_seurat_filtered, reduction = 'umap', group.by = 'Tissue',
+              cols = c('red','green','blue'))
+
+grid.arrange(p1, p2, ncol = 2, nrow = 2)
+
+# perform integration to correct for batch effects ------
+obj.list <- SplitObject(merge_seurat_filtered, split.by = 'Patient_ID')
+for(i in 1:length(obj.list)){
+  obj.list[[i]] <- NormalizeData(object = obj.list[[i]])
+  obj.list[[i]] <- FindVariableFeatures(object = obj.list[[i]])
+}
+
+
+# select integration features
+features <- SelectIntegrationFeatures(object.list = obj.list)
+
+# find integration anchors (CCA)
+anchors <- FindIntegrationAnchors(object.list = obj.list,
+                                  anchor.features = features)
+# integrate data
+seurat.integrated <- IntegrateData(anchorset = anchors)
+
+
+# Scale data, run PCA and UMAP and visualize integrated data
+seurat.integrated <- ScaleData(object = seurat.integrated)
+seurat.integrated <- RunPCA(object = seurat.integrated)
+seurat.integrated <- RunUMAP(object = seurat.integrated, dims = 1:50)
+
+
+p3 <- DimPlot(seurat.integrated, reduction = 'umap', group.by = 'Patient_ID')
+p4 <- DimPlot(seurat.integrated, reduction = 'umap', group.by = 'Tissue',
+              cols = c('red','green','blue'))
+
+
+grid.arrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
